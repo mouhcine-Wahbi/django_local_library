cd6acf85cf4ff6be9a21c25b2b0e59ca0dd0adce
mouhcine-Wahbi <w.mouhcine05@gmail.com>
2025-10-13 18:29:48 +0100

    registering models & creating a superuser

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -108,3 +108,10 @@ class Author(models.Model):
     def __str__(self):
         """String for representing the Model object."""
         return f'{self.last_name}, {self.first_name}'
+    
+class Language(models.Model):
+    """Model representing a book language (e.g. English, French, Arabic)."""
+    name = models.CharField(max_length=200, help_text="Enter the book's natural language (e.g. English, French, Arabic)")
+
+    def __str__(self):
+        return self.name
